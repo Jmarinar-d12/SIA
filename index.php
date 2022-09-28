f3bbb944e14ed620f96ec4a6127ddb91cbc5593a
Jmarinar-d12 <114581817+Jmarinar-d12@users.noreply.github.com>
2022-09-28 19:41:43 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+ <title>Activity1</title>  
+ <body > <center>  
+ <center>
+<table border="2" align="center"  cellspacing="1"cellpadding="110" bgcolor="#d5f4e6"> <td>
+
+<?php
+   
+    echo "<h1>Marinard C. Gatchlian</h1> <b>";
+    echo "BSIT 4A<br> <b>";
+    echo " (System Integration And Architecturebr>";
+    
+    
+?> 
+
+</body>
+</html>
